19a00757c5cc3007982271e9b04ca44a721360b3
Purpleshoes <32720393+Purpleshoes@users.noreply.github.com>
2019-10-09 20:53:00 -0700

    Update AddingMachine.java
    Comments added.

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -4,31 +4,32 @@ public class AddingMachine {
 
 	private int total;
 	private String log;
-	
+	//Init values for integer total and string log
 	public AddingMachine () {
 		total = 0;
 		log = "0";
 	}
-	
+	//Returns integer total of add and subtract methods previously ran
 	public int getTotal () {
 		return total;
 	}
-	
+	//Adds parameter to total. Adds operation to log
 	public void add (int value) {
 		total += value;
 		log += " + " + value;
 	}
-	
+	//Subtracts parameter from total. Adds operation to log
 	public void subtract (int value) {
 		total -= value;
-		log += " + " + value;
+		log += " - " + value;
 	}
-		
+	//Returns recorded add and subtract operations	
 	public String toString () {
 		return log;
 	}
-
+	//Resets total and log to initial values of 0
 	public void clear() {
+		total = 0;
 		log = "0";
 	}
 }
