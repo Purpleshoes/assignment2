2752c74b8946b98e942b861e931e0dab89a8ba77
Purpleshoes <32720393+Purpleshoes@users.noreply.github.com>
2019-10-09 20:46:17 -0700

    Update AddingMachine.java
    Implemented methods. Not yet tested.

--- Assignment2/src/cse360assign2/AddingMachine.java
+++ Assignment2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,32 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String log;
 	
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
+		log = "0";
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		log += " + " + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		log += " + " + value;
 	}
 		
 	public String toString () {
-		return "";
+		return log;
 	}
 
 	public void clear() {
-	
+		log = "0";
 	}
 }
